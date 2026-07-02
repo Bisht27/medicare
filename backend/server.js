12521cfae38b794e44bace4b91ccec633400695f
Deepanshu Bisht <deepanshubisht738@gmail.com>
2026-07-02 11:09:01 +0530

    Add home route to backend

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,13 @@ app.use(express.json());
 
 app.use("/api/appointments", appointmentRoutes);
 
+// Home Route
+app.get("/", (req, res) => {
+    res.send("Medicare Backend is Running 🚀");
+});
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, "0.0.0.0", () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
